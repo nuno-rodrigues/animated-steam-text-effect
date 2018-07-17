ec533488eb0f557e3473824e144a658e7a39d2e6
Nuno Rodrigues <nuno.r.rodrigues@outlook.pt>
2018-07-17 15:37:19 +0100

    Remove JS

--- code.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function(){
-	var nodes = document.querySelectorAll(".blur > *");
-	for(var i=0; i<nodes.length; i++){
-		nodes[i].style.animationDelay = (i*3)+"s";
-	}
-});
